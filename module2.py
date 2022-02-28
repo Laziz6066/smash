2fda676fec54b3b4b2fdf0cf9bb2c52dd0692203
Lazik <lazizsaidov95@gmail.com>
2022-02-28 20:46:28 +0500

    1-commit 2-vetka

--- module2.py
+++ module2.py
@@ -11,3 +11,15 @@ while number != x:
 print('Вы угадали! Число попыток: ', attempt)
 
 print('------------------------ -----------')
+
+number = 0
+x = 20
+attempt = 0
+while number != x:
+    number = int(input('Введите число: '))
+    if number > x:
+        print('Число больше, чем нужно. Попробуйте ещё раз!')
+    else:
+        print('Число меньше, чем нужно. Попробуйте ещё раз!')
+    attempt += 1
+print('Вы угадали! Число попыток: ', attempt)
